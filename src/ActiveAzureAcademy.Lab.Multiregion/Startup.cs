4d936b843f98d09cf8ec9b50156a167b44ff0f51
Peter Örneholm <peter@orneholm.com>
2019-03-28 15:54:55 +0100

    Update to Core 2.2

--- src/ActiveAzureAcademy.Lab.Multiregion/Startup.cs
+++ src/ActiveAzureAcademy.Lab.Multiregion/Startup.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -18,7 +19,8 @@ namespace ActiveAzureAcademy.Lab.Multiregion
 
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            services.AddMvc()
+                    .SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
             services.AddCors();
 
             services.Configure<RegionConfiguration>(Configuration);
